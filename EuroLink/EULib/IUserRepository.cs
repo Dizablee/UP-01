d1540e890a43259bcf523657bd6b4b579d7847d1
Dizablee <mth78931@gmail.com>
2025-10-13 21:43:18 +0300

    сделал тесты функции регистрации пользователя

---
+++ EuroLink/EULib/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EULib
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByEmailAsync(string email);
+        Task<bool> CreateUserAsync(User user);
+        Task<bool> IsEmailExistsAsync(string email);
+    }
+}
