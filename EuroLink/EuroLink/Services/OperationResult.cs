0f0b78b1b4147ed15bdfb1f93a9b611aa1c0bcfe
Dizablee <mth78931@gmail.com>
2025-11-11 08:45:40 +0300

    Создал модель для возврата результатов операций с поддержкой успеха/ошибки

---
+++ EuroLink/EuroLink/Services/OperationResult.cs
@@ -0,0 +1,29 @@
+﻿using EuroLink.Models;
+
+namespace EuroLink.Services
+{
+    public class OperationResult
+    {
+        public bool IsSuccess { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
+        public Phone? Phone { get; set; }
+
+        public static OperationResult Success(Phone phone)
+        {
+            return new OperationResult
+            {
+                IsSuccess = true,
+                Phone = phone
+            };
+        }
+
+        public static OperationResult Failure(string errorMessage)
+        {
+            return new OperationResult
+            {
+                IsSuccess = false,
+                ErrorMessage = errorMessage
+            };
+        }
+    }
+}
