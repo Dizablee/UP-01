f5a1f4b8dab11153e85f16187ae2416c297326f3
Dizablee <mth78931@gmail.com>
2025-11-11 08:39:39 +0300

    Создал интерфейс репозитория для доступа к данным.

---
+++ EuroLink/EuroLink/Services/IPhoneRepository.cs
@@ -0,0 +1,12 @@
+﻿using EuroLink.Models;
+
+namespace EuroLink.Services
+{
+    public interface IPhoneRepository
+    {
+        Task<Phone?> GetPhoneByBrandAndModelAsync(string brand, string model);
+        Task<bool> CreatePhoneAsync(Phone phone);
+        Task<bool> PhoneExistsAsync(string brand, string model);
+        Task<List<Phone>> GetAllPhonesAsync();
+    }
+}
