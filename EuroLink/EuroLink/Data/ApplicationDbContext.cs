cf813e969d6759127feef98d1ae9c49834c1d2c3
Dizablee <mth78931@gmail.com>
2025-11-11 08:35:59 +0300

    Создал и настроил контекст БД с уникальным индексом для комбинации бренд-модель.

---
+++ EuroLink/EuroLink/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EuroLink.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace EuroLink.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Phone> Phones { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Phone>()
+                .HasIndex(p => new { p.Brand, p.Model })
+                .IsUnique();
+        }
+    }
+}
